d49b9593f29a45896222fb1c846860437cd60924
Alexander Bigga <alexander.bigga@slub-dresden.de>
2018-01-28 12:36:06 +0100

    move addStaticFile to TCA/Overrides/sys_templates.php to benefit from caching

--- ext_tables.php
+++ ext_tables.php
@@ -2,11 +2,3 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/SLUB', 'Qucosa: SLUB-Mandant');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/UBL', 'Qucosa: UBL-Mandant');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/MONARCH', 'Qucosa: Chemnitz-Mandant');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/MUSICONN', 'Qucosa: Musiconn');
