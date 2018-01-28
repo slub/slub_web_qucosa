81a6d16cb83e1ff7a57dd1d1581caa790aa2fb41
Alexander Bigga <alexander.bigga@slub-dresden.de>
2018-01-28 18:05:39 +0100

    use proper indents, replace $_EXTKEY variable

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,12 +1,28 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/SLUB', 'Qucosa: SLUB-Mandant');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'slub_web_qucosa',
+    'Configuration/TypoScript/SLUB',
+    'Qucosa: SLUB-Mandant'
+);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/UBL', 'Qucosa: UBL-Mandant');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'slub_web_qucosa',
+    'Configuration/TypoScript/UBL',
+    'Qucosa: UBL-Mandant'
+);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/MONARCH', 'Qucosa: Chemnitz-Mandant');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'slub_web_qucosa',
+    'Configuration/TypoScript/MONARCH',
+    'Qucosa: Chemnitz-Mandant'
+);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/MUSICONN', 'Qucosa: Musiconn');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'slub_web_qucosa',
+    'Configuration/TypoScript/MUSICONN',
+    'Qucosa: Musiconn'
+);
