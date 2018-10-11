5cf43dd8f23004ecf522497e3be4c54bf10788f4
Alexander Bigga <alexander.bigga@slub-dresden.de>
2018-10-11 12:43:59 +0200

    tweek a little date search fields in extended search form

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -5,8 +5,10 @@ if (!defined('TYPO3_MODE')) {
 
 if (TYPO3_MODE === 'BE') {
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile (
-   'slub_web_qucosa',
-   'Configuration/TsConfig/Page/setup.txt',
-   'EXT:slub_web_qucosa: MUSICONN Page TS');
+    if (version_compare(TYPO3_version, '7.6.0', '>=')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile (
+       'slub_web_qucosa',
+       'Configuration/TsConfig/Page/setup.txt',
+       'EXT:slub_web_qucosa: MUSICONN Page TS');
+    }
 }
