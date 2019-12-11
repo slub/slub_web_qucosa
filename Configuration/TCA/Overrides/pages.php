648b91499cfc03e96d898a63275710efe64cac1b
Alexander Bigga <alexander.bigga@slub-dresden.de>
2019-12-11 13:56:28 +0100

    remove version_compare

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -4,11 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 if (TYPO3_MODE === 'BE') {
-
-    if (version_compare(TYPO3_version, '7.6.0', '>=')) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile (
        'slub_web_qucosa',
        'Configuration/TsConfig/Page/setup.txt',
        'EXT:slub_web_qucosa: MUSICONN Page TS');
-    }
 }
