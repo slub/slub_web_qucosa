685d57e5aa8f626430107e5a058e330048ce683b
Alexander Bigga <alexander.bigga@slub-dresden.de>
2019-12-11 09:38:08 +0100

    another realurl tweak

--- ext_localconf.php
+++ ext_localconf.php
@@ -30,47 +30,47 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVar
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['slub.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['slub.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['slub.qucosa.de']['fixedPostVars']['20'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['slub.qucosa.de']['fixedPostVars']['24'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['monarch.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['monarch.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['monarch.qucosa.de']['fixedPostVars']['118'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['monarch.qucosa.de']['fixedPostVars']['106'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tubaf.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tubaf.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tubaf.qucosa.de']['fixedPostVars']['73'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tubaf.qucosa.de']['fixedPostVars']['68'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['ul.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['ul.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['ul.qucosa.de']['fixedPostVars']['40'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['ul.qucosa.de']['fixedPostVars']['46'] = 'apiConfiguration';
 
 // we need ubl for compatibility reasons; use ul.qucosa.de as default
 //$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['ubl.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['ul.qucosa.de'];
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['hzdr.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['hzdr.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['hzdr.qucosa.de']['fixedPostVars']['95'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['hzdr.qucosa.de']['fixedPostVars']['87'] = 'apiConfiguration';
 //$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['fzd.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['hzdr.qucosa.de'];
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tud.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tud.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tud.qucosa.de']['fixedPostVars']['210'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['tud.qucosa.de']['fixedPostVars']['198'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['htw-dresden.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['htw-dresden.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['htw-dresden.qucosa.de']['fixedPostVars']['129'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['htw-dresden.qucosa.de']['fixedPostVars']['124'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['diu.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['diu.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['diu.qucosa.de']['fixedPostVars']['147'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['diu.qucosa.de']['fixedPostVars']['142'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['musiconn.qucosa.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['musiconn.qucosa.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['musiconn.qucosa.de']['fixedPostVars']['189'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['musiconn.qucosa.de']['fixedPostVars']['195'] = 'apiConfiguration';
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['publish.fid-move.de']['fixedPostVars'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fixedPostVars'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['publish.fid-move.de'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['publish.fid-move.de']['fixedPostVars']['60'] = 'landingPageConfiguration';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['publish.fid-move.de']['fixedPostVars']['50'] = 'apiConfiguration';
 
