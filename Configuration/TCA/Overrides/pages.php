bf22e154ab4b2dab6235a381692bd50bce2d961b
Eliseo Malo <ema@xima.de>
2021-05-06 11:12:15 +0200

    [WiP] Add new Content Element HeaderTeaser

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,14 +3,28 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+/**
+ * Temporary variables
+ */
+$extensionKey = 'slub_web_qucosa';
+
+/**
+ * Default PageTS for Qucosa
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    $extensionKey,
+    'Configuration/PageTS/All.txt',
+    'Qucosa'
+);
+
 if (TYPO3_MODE === 'BE') {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile (
-       'slub_web_qucosa',
+        $extensionKey,
        'Configuration/TsConfig/Page/MUSICONN/setup.txt',
        'EXT:slub_web_qucosa: MUSICONN Page TS');
 
        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile (
-      'slub_web_qucosa',
+      $extensionKey,
       'Configuration/TsConfig/Page/FIDMOVE/setup.txt',
       'EXT:slub_web_qucosa: FID-Move Page TS');
 }
