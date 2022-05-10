86fec59b97c651229722b3b607137d5a9e362079
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2022-05-10 15:07:42 +0200

    Require typo3/cms-core
    TYPO3 module typo3/cms is no longer supported.
    Also updates ext_emconf TYPO3 version dependency.

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.30-8.7.99',
+			'typo3' => '9.5.0-9.5.31',
 		),
 		'conflicts' => array(
 		),
