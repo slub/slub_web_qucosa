739e829ee56d2b5023163e2bde679957415b6485
Thomas Jung <thomas@jung.digital>
2023-06-19 10:47:04 +0200

    Add custom RTE config for Musiconn

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,6 +10,7 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['fileName']['
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['qucosa'] = 'EXT:slub_web_qucosa/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['musiconn'] = 'EXT:slub_web_qucosa/Configuration/RTE/MusiconnPublish/Default.yaml';
 
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['www.qucosa.de']['cache']['banUrlsRegExp'] = '/type=|eID=|tx_dpf_frontendsearch|tx_dlf|(?:^|\?|&)q=/';
